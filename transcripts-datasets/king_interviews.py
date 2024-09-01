c14411562f93c9fd7f89a971776c071ecea36ecb
bour278 <josephaldojbbr@gmail.com>
2024-09-01 00:26:35 -0400

    updating transcripts and adding base model

--- transcripts-datasets/king_interviews.py
+++ transcripts-datasets/king_interviews.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+import re
 
 BASE_URL = "https://transcripts.cnn.com/show/lkl"
 OUTPUT_DIR = "./transcripts"
@@ -18,6 +19,15 @@ def parse_transcripts(page_content):
         transcripts.append(link['href'])
     return transcripts
 
+def extract_transcript_content(page_content):
+    soup = BeautifulSoup(page_content, 'html.parser')
+    sub_head = soup.select_one('p.cnnTransSubHead').get_text(strip=True)
+    body_texts = [p.get_text(strip=True) for p in soup.select('p.cnnBodyText')]
+    return sub_head, "\n".join(body_texts)
+
+def sanitize_filename(filename):
+    return re.sub(r'[\\/*?:"<>|]', "", filename)
+
 def save_transcript(content, filename):
     with open(os.path.join(OUTPUT_DIR, filename), 'w', encoding='utf-8') as file:
         file.write(content)
@@ -34,7 +44,9 @@ def scrape_transcripts():
         for link in transcript_links:
             transcript_url = f"https://transcripts.cnn.com{link}"
             transcript_content = fetch_page(transcript_url)
-            save_transcript(transcript_content, f"interview_{page_num}_{transcript_links.index(link)}.txt")
+            sub_head, body_text = extract_transcript_content(transcript_content)
+            filename = sanitize_filename(f"{sub_head.replace(' ', '_')}.txt")
+            save_transcript(body_text, filename)
         
         next_page = BeautifulSoup(page_content, 'html.parser').select_one('a[href*="?start_fileid="]')
         if next_page:
